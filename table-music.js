ce25c9ce8a7c9ddfb301411996983308078d6cae
mario <panggihmario@gmail.com>
2018-07-20 15:18:37 +0700

    show data from db & upload

--- table-music.js
+++ table-music.js
@@ -1,10 +1,32 @@
 Vue.component('list-music',{
-    template : `  
-      <tr>
-          <td> {{music.title}}</td>
-          <td> {{music.artist}}</td>
-          <td> {{music.category}}</td> 
-      </tr>
- `,
-    props :['music']
+    template : `
+        <table class="centered">
+            <thead>
+                <tr>
+                <th></th>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Category</th>
+                <th>Share</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="music in musics">
+                    <td>
+                        <audio controls> 
+                            <source v-bind:src="music.url" type="audio/mpeg">
+                          Your browser does not support the audio element.
+                        </audio> 
+                    </td>
+                    <td>
+                    {{music.title}}
+                    </td>
+                    <td>{{music.singer}} </td>
+                    <td>{{music.genre}}</td>
+                    <td><i class="fab fa-twitter"></i></td>
+                </tr>
+            </tbody>
+        </table>
+    `,
+    props :['musics']
 })
