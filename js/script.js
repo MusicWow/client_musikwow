b2e638d0a8f2a61484b7b0a37435285250d65d03
handipriyono <handimacbook@gmail.com>
2018-07-20 15:31:32 +0700

    update search features ytbe

--- js/script.js
+++ js/script.js
@@ -1,29 +1,51 @@
 new Vue({
-    el : '#app',
-    data : {
-        musics : [
-            {
-                title :'Best Part',
-                artist : 'Daniel Caesaar',
-                category : 'rock'
+  el: '#app',
+  data: {
+    musics: [
+      {
+        title: 'Best Part',
+        artist: 'Daniel Caesaar',
+        category: 'rock'
 
-            },{
-                title : 'Let me',
-                artist : 'zayn',
-                category : 'rock'
-            },{
-                title : 'Let me',
-                artist : 'zayn',
-                category : 'rock'
-            },{
-                title : 'Let me',
-                artist : 'zayn',
-                category : 'rock'
-            },{
-                title : 'Let me',
-                artist : 'zayn',
-                category : 'rock'
+            }, {
+        title: 'Let me',
+        artist: 'zayn',
+        category: 'rock'
+            }, {
+        title: 'Let me',
+        artist: 'zayn',
+        category: 'rock'
+            }, {
+        title: 'Let me',
+        artist: 'zayn',
+        category: 'rock'
+            }, {
+        title: 'Let me',
+        artist: 'zayn',
+        category: 'rock'
             }
-        ]
+        ],
+    values: "",
+    seen: true,
+    filterSongs: 'ali'
+  },
+  methods: {
+
+    findMusic: function () {
+      this.seen = false
+      const self = this
+      axios.post('http://localhost:3000/musics/search', {
+          value: this.values
+        })
+        .then(function (songs) {
+
+          self.filterSongs = songs.data
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+
     }
+
+  }
 })
